df0ee87159da9e6769049e229093c5550987f1f3
unknown <mdmehedyh55@gamil.com>
2022-03-11 10:20:11 +0600

    call mongoDB

--- src/page/Home/Home/Home.js
+++ src/page/Home/Home/Home.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import About from '../About/About';
 import HomeHeader from '../HomeHeader/HomeHeader'
 import Skills from '../Skills/Skills';
 import Education from '../Education/Education';
+import axios from 'axios';
 
 const Home = () => {
+    useEffect(() => {
+        axios('https://hidden-savannah-18290.herokuapp.com/projects')
+        .then()
+    },[])
     return (
         <div className="container">
             <HomeHeader/>
